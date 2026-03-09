9df885dfab68ecb46e74aa4e69aa1bf80764823e
jp055ung <jpgames055@gmail.com>
2026-03-09 14:58:11 -0300

    Substituição

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import { fileURLToPath } from "url";
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: { alias: { "@": path.resolve(__dirname, "client/src") } },
-  root: ".",
-  build: { outDir: "dist/client", rollupOptions: { input: path.resolve(__dirname, "index.html") } },
-  server: { proxy: { "/api": "http://localhost:3000" } },
+  root: '.',
+  build: {
+    outDir: 'dist/client',
+  },
+  server: {
+    proxy: { '/api': 'http://localhost:3000' },
+  },
 });
